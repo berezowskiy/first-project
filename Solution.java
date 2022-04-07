d28dc99cffa26af00f5dc05a088aea34b658f679
Березовский Дмитрий Александрович <dberezovskiy@phoenixit.ru>
2022-04-07 21:29:26 +0300

    added printName in main

--- Solution.java
+++ Solution.java
@@ -1,6 +1,11 @@
 public class Solution {
 
     public static void main(String[] args) {
+        String name = "Woody";
+        printName(name);
+    }
 
+    public static void printName(String name) {
+        System.out.println("Entered name is: " + name);
     }
 }
